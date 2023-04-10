df7e67e5515272c57f9a8c1f84b1ef5e0f21b734
Jonathan Yazdanpanah <75292585+jyazdan@users.noreply.github.com>
2023-04-10 12:08:00 -0400

    final changes

--- index.js
+++ index.js
@@ -1,30 +1,67 @@
-import characterData from "./data.js";
-import Character from "./character.js";
+import characterData from './data.js'
+import Character from './Character.js'
 
-function render() {
-  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
-  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
+let monstersArray = ["orc", "demon", "goblin"]
+let isWaiting = false
+
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
 }
 
-function attack(){
-  wizard.getDiceHtml()
-  orc.getDiceHtml()
-  wizard.takeDamage(orc.currentDiceScore)
-  orc.takeDamage(wizard.currentDiceScore)
- render()
-
- if (wizard.dead || orc.dead) {
-  endGame()
- }
+function attack() {
+    if(!isWaiting){
+        wizard.setDiceHtml()
+        monster.setDiceHtml()
+        wizard.takeDamage(monster.currentDiceScore)
+        monster.takeDamage(wizard.currentDiceScore)
+        render()
+        
+        if(wizard.dead){
+            endGame()
+        }
+        else if(monster.dead){
+            isWaiting = true
+            if(monstersArray.length > 0){
+                setTimeout(()=>{
+                    monster = getNewMonster()
+                    render()
+                    isWaiting = false
+                },1500)
+            }
+            else{
+                endGame()
+            }
+        }    
+    }
 }
 
-function endGame(){
-  const endMessage = wizard.dead && orc.dead ? 'No victors - all creatures are dead' : orc.dead ? 'Wizard wins' : 'The Orc is Victorious'
-  console.log(endMessage)
+function endGame() {
+    isWaiting = true
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+            "The monsters are Victorious"
+
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+        setTimeout(()=>{
+            document.body.innerHTML = `
+                <div class="end-game">
+                    <h2>Game Over</h2> 
+                    <h3>${endMessage}</h3>
+                    <p class="end-emoji">${endEmoji}</p>
+                </div>
+                `
+        }, 1500)
 }
 
 document.getElementById("attack-button").addEventListener('click', attack)
 
-const wizard = new Character(characterData.hero);
-const orc = new Character(characterData.monster);
-render();
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml()
+}
+
+const wizard = new Character(characterData.hero)
+let monster = getNewMonster()
+render()
