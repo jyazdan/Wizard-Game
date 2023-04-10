5bdd296e1cf898fb9e477d580db11b3ec560970d
Jonathan Yazdanpanah <75292585+jyazdan@users.noreply.github.com>
2023-04-09 23:38:09 -0400

    added end game features

--- index.js
+++ index.js
@@ -1,58 +1,30 @@
-const hero = {
-    elementId: "hero", 
-    name: "Wizard",
-    avatar: "images/wizard.png",
-    health: 60,
-    diceCount: 3
+import characterData from "./data.js";
+import Character from "./character.js";
+
+function render() {
+  document.getElementById("hero").innerHTML = wizard.getCharacterHtml();
+  document.getElementById("monster").innerHTML = orc.getCharacterHtml();
 }
 
-const monster = {
-    elementId: "monster",
-    name: "Orc",
-    avatar: "images/orc.png",
-    health: 10,
-    diceCount: 1
+function attack(){
+  wizard.getDiceHtml()
+  orc.getDiceHtml()
+  wizard.takeDamage(orc.currentDiceScore)
+  orc.takeDamage(wizard.currentDiceScore)
+ render()
+
+ if (wizard.dead || orc.dead) {
+  endGame()
+ }
 }
 
-function Character(data){
-  this.elementId = data.elementId
-  this.name = data.name
-  this.avatar = data.avatar
-  this.health = data.health
-  this.diceCount = data.diceCount
-  this.getCharacterHtml = () => {
-   const {elementId, name, avatar, health, diceCount } = this;
-   const diceHtml = getDiceHtml(diceCount)
-    
-    document.getElementById(elementId).innerHTML = 
-    `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}" />
-        <div class="health">health: <b>${health}</b></div>
-        <div class="dice-container">
-            ${diceHtml}
-        </div>
-    </div>`; 
-  }
+function endGame(){
+  const endMessage = wizard.dead && orc.dead ? 'No victors - all creatures are dead' : orc.dead ? 'Wizard wins' : 'The Orc is Victorious'
+  console.log(endMessage)
 }
 
+document.getElementById("attack-button").addEventListener('click', attack)
 
-function getDiceRollArray(diceCount){
-  const result = new Array(diceCount).fill(0).map(() => {
-    return Math.floor(Math.random() * 6) + 1
-  })
-  console.log(result)
-  return result
-}
-
-function getDiceHtml(diceCount){
-  return getDiceRollArray(diceCount).map((diceValue) => {
-    return `<div class="dice">${diceValue}</div>`
-  }).join(' ')
-}
-
-const wizard = new Character(hero)
-const orc = new Character(monster)
-
-wizard.getCharacterHtml()
-orc.getCharacterHtml()
+const wizard = new Character(characterData.hero);
+const orc = new Character(characterData.monster);
+render();
